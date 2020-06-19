729ee063fc897e2729f372f95ce7e79bfad3dbb2
hupeng <bankhp@163.com>
2020-06-19 16:40:57 +0800

    edit model config admin config

--- apps/datacloud/admin.py
+++ apps/datacloud/admin.py
@@ -27,12 +27,53 @@ class ChkInfoAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         obj.sync_flag = False
-        chk_info = ChkInfo.objects.filter(db_name=obj.db_name)
-        if chk_info:
-            db_name_count = chk_info.count()
-            obj.chk_seq = db_name_count + 1
-        else:
-            obj.chk_seq = 1
-        chn_id = ChannelInfo.objects.get(db_name=obj.db_name).chn_id
-        obj.chk_id = chn_id + obj.chk_seq
+        if not change:
+            chk_info = ChkInfo.objects.filter(db_name=obj.db_name)
+            if chk_info:
+                db_name_count = chk_info.count()
+                obj.chk_seq = db_name_count + 1
+            else:
+                obj.chk_seq = 1
+            chn_id = ChannelInfo.objects.get(db_name=obj.db_name).chn_id
+            obj.chk_id = chn_id + obj.chk_seq + 10000  # 检测编号规则是110001、110002
         return super(ChkInfoAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(SyncTaskInfo)
+class SyncTaskInfoAdmin(admin.ModelAdmin):
+    list_display = ('db_name', 'chk_name', 'tab_name', 'exp_method', 'zl_info', 'outfile_type', 'his_flag', 'val_flag')
+    fields = ('db_name', 'chk_name', 'tab_name', 'exp_method', 'zl_info', 'zl_col', 'ftp_file',
+              'outfile_type', 'date_type', 'out_path', 'load_method', 'load_tab_tmp',
+              'load_tab_mir', 'month_flag', 'his_flag', 'val_flag')
+
+    def save_model(self, request, obj, form, change):
+        obj.sync_flag = False
+        chn_id = ChannelInfo.objects.get(db_name=obj.db_name).chn_id
+        if not change:
+            task_info = SyncTaskInfo.objects.filter(db_name=obj.db_name)
+            if task_info:
+                task_count = task_info.count()
+                obj.sync_id = chn_id + 20000 + task_count + 1
+                obj.load_id = chn_id + 40000 + task_count + 1
+            else:
+                obj.sync_id = chn_id + 20001
+                obj.load_id = chn_id + 40001
+        return super(SyncTaskInfoAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(PushTaskInfo)
+class PushTaskInfoAdmin(admin.ModelAdmin):
+    list_display = ('db_name', 'tab_name', 'file_type', 'code_page', 'path', 'val_flag')
+    fields = ('db_name', 'tab_name', 'path', 'file_type', 'code_page', 'separator', 'delimiter', 'val_flag')
+
+    def save_model(self, request, obj, form, change):
+        obj.sync_flag = False
+        chn_id = ChannelInfo.objects.get(db_name=obj.db_name).chn_id
+        if not change:
+            task_info = PushTaskInfo.objects.filter(db_name=obj.db_name)
+            if task_info:
+                task_count = task_info.count()
+                obj.push_id = chn_id + 60000 + task_count + 1
+            else:
+                obj.push_id = chn_id + 60001
+        return super(PushTaskInfoAdmin, self).save_model(request, obj, form, change)
--- apps/datacloud/models.py
+++ apps/datacloud/models.py
@@ -132,7 +132,7 @@ class SyncTaskInfo(models.Model):
                                   help_text='自动生成的隐藏列', unique=True)
 
     def __str__(self):
-        return self.db_name
+        return self.tab_name
 
     class Meta:
         verbose_name = '数据同步加载任务'
@@ -143,6 +143,7 @@ class PushTaskInfo(models.Model):
     """
     数据推送任务信息表
     """
+    db_name = models.ForeignKey(ChannelInfo, on_delete=models.DO_NOTHING, verbose_name='源系统库名')
     tab_name = models.CharField(verbose_name='表名', max_length=128,
                                 help_text='实际存储表名，非上游源系统表名')
     path = models.CharField(verbose_name='推送目录', max_length=128)
@@ -150,8 +151,10 @@ class PushTaskInfo(models.Model):
                                  choices=(('ixf', 'ixf'), ('txt', 'txt')))
     code_page = models.CharField(verbose_name='编码格式', max_length=10,
                                  choices=(('1386', '1386'), ('1208', '1208')))
-    separator = models.CharField(verbose_name='导出字段分隔符', max_length=10, help_text='不指定则默认为，')
-    delimiter = models.CharField(verbose_name='字段限定符', max_length=10, help_text='不指定则默认为"')
+    separator = models.CharField(verbose_name='导出字段分隔符', max_length=10, null=True, blank=True,
+                                 help_text='不指定则默认为，')
+    delimiter = models.CharField(verbose_name='字段限定符', max_length=10, null=True, blank=True,
+                                 help_text='不指定则默认为"')
     val_flag = models.BooleanField(verbose_name='有效标识', default=True)
     sync_flag = models.BooleanField(verbose_name='同步标识', default=False)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
