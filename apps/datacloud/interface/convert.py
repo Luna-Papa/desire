9275663bf5b878face140005ca172094bcd97020
hupeng <bankhp@163.com>
2020-07-24 18:01:13 +0800

    update sms and rename file

--- apps/datacloud/interface/convert.py
+++ apps/datacloud/interface/convert.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     """
     command1 = os.path.join(INTERFACE_DIR, 'NewRecordConvertToConfig.py')  # 添加新记录时，用于更新后台调度中间表
     command2 = os.path.join(INTERFACE_DIR, 'ValidRecordConvertToETL.py')  # 把记录置为无效时，用于更新后台调度表
-    command3 = os.path.join(INTERFACE_DIR, 'ConvertToETL.py')  # 添加和修改记录时，用于更新后台配置表
+    command3 = os.path.join(INTERFACE_DIR, 'ConvertToConfig.py')  # 添加和修改记录时，用于更新后台配置表
     command4 = os.path.join(INTERFACE_DIR, 'initializeETL.py')  # 最终调度中间表处理到调度结果表
 
     os.system(eval('f' + '"' + 'python {command1}' + '"'))
